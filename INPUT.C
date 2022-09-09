1ad8a2a3822c849d08be77b9e35ebff97d1e9059
Meeran114 <111926716+Meeran114@users.noreply.github.com>
2022-09-09 10:28:58 -0700

    Add files via upload

---
+++ INPUT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+float b;
+printf("Please enter a Integer and float value");
+scanf("%d%f",&a,&b);
+printf("The Integer value is %d The float value is %f",a,b);
+getch();
+return 0;
+}
---
+++ OUTPUT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("Welcome to C programming");
+int age=19;
+char name[20]="Meeran";
+printf("\nMy Name is %s",name);
+printf("\nMy Age is %d",age);
+getch();
+return 0;
+}
