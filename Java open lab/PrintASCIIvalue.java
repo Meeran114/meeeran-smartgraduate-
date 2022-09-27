ef5b1413bff12f562e63849792fa31ef359d9946
Meeran114 <111926716+Meeran114@users.noreply.github.com>
2022-09-27 09:13:20 +0530

    Add files via upload

---
+++ Java open lab/PrintASCIIvalue.java
@@ -0,0 +1,13 @@
+
+public class PrintASCIIvalue {
+
+	public static void main(String[] args) {
+char ch='H';
+int ascii=ch;
+int castascii=(int)ch;
+System.out.println("The ASCII value of "+ch+" is "+ascii);
+System.out.println("The ASCII value of "+ch+" is "+castascii);
+
+	}
+
+}
