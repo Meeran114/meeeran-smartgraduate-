4deae2065077b0f50cc4c9bdad08ab0c7b854872
Meeran114 <111926716+Meeran114@users.noreply.github.com>
2022-09-27 09:44:40 +0530

    Add files via upload

---
+++ Java open lab/QuotientRemainder.java
@@ -0,0 +1,10 @@
+
+public class QuotientRemainder {
+public static void main(String[] args) {
+	int divisor=25,dividend=4;
+	int quotient=divisor/dividend;
+	int remainder=divisor%dividend;
+	System.out.println("The Quotient is "+ quotient);
+	System.out.println("The Remainder is "+ remainder);
+}
+}
