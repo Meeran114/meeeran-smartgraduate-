27d90b9907f62e1937e86e081165fa59125003e3
Meeran114 <111926716+Meeran114@users.noreply.github.com>
2022-09-29 00:07:21 +0530

    Add files via upload

---
+++ Java open lab/SwaptwonumbersExercise.java
@@ -0,0 +1,17 @@
+
+public class SwaptwonumbersExercise {
+	public static void main(String[] args) {
+		float a=4.6f,b=9.8f;
+		System.out.println("Before Swap");
+		System.out.println("First Number "+a);
+		System.out.println("Second Number "+b);
+		float temporary=a;
+		a=b;
+		b=temporary;
+		System.out.println("After Swap");
+		System.out.println("First Number "+a);
+		System.out.println("Second Number "+b);
+		
+	}
+
+}
---
+++ Java open lab/Vowel.java
@@ -0,0 +1,16 @@
+
+public class Vowel {
+
+	public static void main(String[] args) {
+	char a='i';
+	if (a=='a' ||a=='i' ||a=='o' ||a=='u' ||a=='e')
+	{
+		System.out.println(a+" is a Vowel");
+	}
+	else
+	{
+		System.out.println(a+" is a Consonant");
+	}
+	}
+
+}
